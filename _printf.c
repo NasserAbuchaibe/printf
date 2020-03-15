51c32e25b42f188a6bcbf4b9736f108e01670a15
NasserAbuchaibe <nasser_abuchaibe@hotmail.com>
2020-03-15 22:20:09 +0000

    function v1 printf

---
+++ _printf.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+
+/**
+ * _printf - produces output according to a format
+ * @format: first variable function argument
+ * return: Printed character numbers or 
+ * a negative value if an error occurs
+ */
+int _printf(const char *format, ...)
+{
+}
