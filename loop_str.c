9c2cebf8f2b3ea200e07216d18c68267bfd340f5
kimha1030 <1478@holbertonschool.com>
2020-03-16 18:29:57 -0500

    Update Function loop_str

--- loop_str.c
+++ loop_str.c
@@ -9,26 +9,28 @@
  */
 int loop_str(const char *format, prt_f func[], va_list p)
 {
-	int x = 0, y = 0, val_ret;
+	int x = 0, y = 0, val_ret, count = 0;
 
 	for (x = 0; format[x] != '\0'; x++)
 	{
-		if (format[x] != '%' && format[x] != "\\")
+		if (format[x] != '%' && format[x] != '\\')
 			return (_write(format[x]));
-		else if (format[x] == '%' || format[x] == "\\")
+		else if (format[x] == '%' || format[x] == '\\')
 		{
 			for (y = 0; func[y].prt != NULL; y++)
 			{
-				if (format[x + 1] == func[y].prt)
+				if (format[x + 1] == func[y].prt[0])
 				{
 					val_ret = func[y].f(p);
 					if (val_ret == -1)
 						return (-1);
-					return (val_ret);
+					count += val_ret;
+					break;
 				}
 			}
 			if (format[x + 2] != '\0')
 				x = x + 2;
 		}
 	}
+	return (count);
 }
