df47d37563d82e3e89dae1a2669c4b736a0880cc
kimha1030 <1478@holbertonschool.com>
2020-03-16 13:48:40 -0500

    Update header file

--- holberton.h
+++ holberton.h
@@ -4,14 +4,18 @@
 #include <stdarg.h>
 #include <unistd.h>
 
+int _write(char c);
 int _printf(const char *format, ...);
+int loop_str(const char *format, prt_f func[], va_list p);
+int f_char(va_list p);
+int f_string(va_list p);
+int f_percent(va_list p);
+int f_backslash(va_list p);
 
 typedef struct prt
 {
 	char *prt;
 	int (*f)(va_list);
 } prt_f;
-
-int loop_str(const char *format, prt_f func[], va_list p);
 
 #endif
