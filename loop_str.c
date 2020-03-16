92cb6658fc2ead0479696f97d49a9604e01439eb
NasserAbuchaibe <nasser_abuchaibe@hotmail.com>
2020-03-16 19:16:54 +0000

    loop through and evaluate each string element

---
+++ loop_str.c
@@ -0,0 +1,34 @@
+#include "halberton.h"
+
+/**
+ * loop_str - loop through and evaluate each string element
+ * @format: first argument which is a constant string
+ * @func: function arrays
+ * @p: variable argument list
+ * Return: number int the number of characters printed
+ */
+int loop_str(const char *format, prt_f func[], va_list p)
+{
+	int x = 0, y = 0, val_ret;
+
+	for (x = 0; format[x] != '\0'; x++)
+	{
+		if (format[x] != '%' && format[x] != "\\")
+			return (_write(format[x]));
+		else if (format[x] == '%' || format[x] == "\\")
+		{
+			for (y = 0; func[y].prt != NULL; y++)
+			{
+				if (format[x + 1] == func[y].prt)
+				{
+					val_ret = func[y].f(p);
+					if (val_ret == -1)
+						return (-1);
+					return (val_ret);
+				}
+			}
+			if (format[x + 2] != '\0')
+				x = x + 2;
+		}
+	}
+}
