2d4d70a9fd6bd3e3cec0bf26cae306b6c441517d
kimha1030 <1478@holbertonschool.com>
2020-03-16 14:45:08 -0500

    Update Functions of project printf

--- functions.c
+++ functions.c
@@ -13,7 +13,7 @@ int _write(char c)
 
 /**
  * f_char - Function that prints a char using function _write
- * @p: Variable argument list
+ * @p: Variable list argument
  * Return: On success 1.
  * On error, -1 is returned.
  */
@@ -26,50 +26,50 @@ int f_char(va_list p)
 }
 /**
  * f_string - Function that prints a string using function _write
- * @p: Variable argument list.
+ * @p: Variable list argument
  * Return: On success 1.
  * On error, -1 is returned.
  */
 int f_string(va_list p)
 {
 	char *s;
-	int x;
 
 	s = va_arg(p, char*);
 	if (s == NULL)
 	{
 		return (-1);
 	}
-	for (x = 0 ; s != '\0' ; x++)
+	while (*s)
 	{
+		s++;
 		return (_write(s[x]));
 	}
 }
 
 /**
  * f_percent - Function that prints a symbol of percent using function _write
- * @p: Variable argument list
+ * @p: Variable list argument
  * Return: On success 1.
  * On error, -1 is returned.
  */
 int f_percent(va_list p)
 {
-	int x;
-
+	char *x;
+	x = va_arg(p, int);
 	if (format[x + 1] == '%')
 		return (_write("%"));
 }
 
 /**
  * f_backslash - Function that prints a backslash using function _write
- * @p: Variable argument list.
+ * @p: Variable list argument
  * Return: On success 1.
  * On error, -1 is returned.
  */
 int f_backslash(va_list p)
 {
-	int x;
-
+	char *x;
+        x = va_arg(p, int);
 	if (format[x + 1] == 'b')
 		return (_write('\b'));
 	else if (format[x + 1] == 'f')
