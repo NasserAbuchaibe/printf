310d21543ec0aa773024389bf98bff2762a9cb7d
kimha1030 <1478@holbertonschool.com>
2020-03-16 13:23:08 -0500

    Functions of project printf

--- functions.c
+++ functions.c
@@ -1,7 +1,4 @@
 #include "holberton.h"
-#include <stdarg.h>
-#include <stdio.h>
-#include <stdarg.h>
 
 /**
  * _write - Write the character c to stdout
@@ -17,29 +14,76 @@ int _write(char c)
 /**
  * f_char - Function that prints a char using function _write
  * @p: Variable argument list
- * Return:
+ * Return: On success 1.
+ * On error, -1 is returned.
  */
 
 int f_char(va_list p)
 {
+	char ch = va_arg(p, int);
 
+	return (_write(ch));
 }
 /**
  * f_string - Function that prints a string using function _write
  * @p: Variable argument list.
- * Return:
+ * Return: On success 1.
+ * On error, -1 is returned.
  */
 int f_string(va_list p)
 {
+	char *s;
+	int x;
 
+	s = va_arg(p, char*);
+	if (s == NULL)
+	{
+		return (-1);
+	}
+	for (x = 0 ; s != '\0' ; x++)
+	{
+		return (_write(s[x]));
+	}
 }
 
 /**
  * f_percent - Function that prints a symbol of percent using function _write
  * @p: Variable argument list
- * Return:
+ * Return: On success 1.
+ * On error, -1 is returned.
  */
 int f_percent(va_list p)
 {
+	int x;
 
+	if (format[x + 1] == '%')
+		return (_write("%"));
+}
+
+/**
+ * f_backslash - Function that prints a backslash using function _write
+ * @p: Variable argument list
+ * Return: On success 1.
+ * On error, -1 is returned.
+ */
+int f_backslash(va_list p)
+{
+	int x;
+
+	if (format[x + 1] == 'b')
+		return (_write('\b'));
+	else if (format[x + 1] == 'f')
+		return (_write('\f'));
+	else if (format[x + 1] == 'n')
+		return (_write('\n'));
+	else if (format[x + 1] == 't')
+		return (_write('\t'));
+	else if (format[x + 1] == 'v')
+	{
+		return (_write('\v'));
+	}
+	else
+	{
+		return (_write(format[x + 1]));
+	}
 }
