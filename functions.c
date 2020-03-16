6234b01c7569851beb9c475e2bcd9e0b8049e937
kimha1030 <1478@holbertonschool.com>
2020-03-16 18:31:47 -0500

    Update functions of printf project

--- functions.c
+++ functions.c
@@ -17,13 +17,12 @@ int _write(char c)
  * Return: On success 1.
  * On error, -1 is returned.
  */
-
 int f_char(va_list p)
 {
-	char ch = va_arg(p, int);
-
-	return (_write(ch));
+	_write(va_arg(p, int));
+	return (1);
 }
+
 /**
  * f_string - Function that prints a string using function _write
  * @p: Variable list argument
@@ -33,17 +32,18 @@ int f_char(va_list p)
 int f_string(va_list p)
 {
 	char *s;
+	int x;
 
 	s = va_arg(p, char*);
 	if (s == NULL)
 	{
 		return (-1);
 	}
-	while (*s)
+	for (x = 0 ; s[x] != '\0' ; x++)
 	{
-		s++;
-		return (_write(s[x]));
+		_write(s[x]);
 	}
+	return (x);
 }
 
 /**
@@ -52,38 +52,8 @@ int f_string(va_list p)
  * Return: On success 1.
  * On error, -1 is returned.
  */
-int f_percent(va_list p)
+int f_percent(__attribute__((unused))va_list p)
 {
-	char *x;
-	x = va_arg(p, int);
-	if (format[x + 1] == '%')
-		return (_write("%"));
-}
-
-/**
- * f_backslash - Function that prints a backslash using function _write
- * @p: Variable list argument
- * Return: On success 1.
- * On error, -1 is returned.
- */
-int f_backslash(va_list p)
-{
-	char *x;
-        x = va_arg(p, int);
-	if (format[x + 1] == 'b')
-		return (_write('\b'));
-	else if (format[x + 1] == 'f')
-		return (_write('\f'));
-	else if (format[x + 1] == 'n')
-		return (_write('\n'));
-	else if (format[x + 1] == 't')
-		return (_write('\t'));
-	else if (format[x + 1] == 'v')
-	{
-		return (_write('\v'));
-	}
-	else
-	{
-		return (_write(format[x + 1]));
-	}
+	_write('%');
+	return (1);
 }
