5205f8e141b5380808d573460a693d5eb879ab37
NasserAbuchaibe <nasser_abuchaibe@hotmail.com>
2020-03-17 05:54:44 +0000

    fix number of functions

---
+++ _write.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ * _write - Write the character c to stdout
+ * @c: The character to print
+ * Return: On success 1.
+ * On error, -1 is returned.
+ */
+int _write(char c)
+{
+	return (write(1, &c, 1));
+}
--- functions.c
+++ functions.c
@@ -1,15 +1,5 @@
 #include "holberton.h"
 
-/**
- * _write - Write the character c to stdout
- * @c: The character to print
- * Return: On success 1.
- * On error, -1 is returned.
- */
-int _write(char c)
-{
-	return (write(1, &c, 1));
-}
 
 /**
  * f_char - Function that prints a char using function _write
