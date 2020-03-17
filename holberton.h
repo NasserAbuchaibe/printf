4e60c1ac123ce2b24903ef978d97d619fabeaf00
NasserAbuchaibe <nasser_abuchaibe@hotmail.com>
2020-03-17 01:52:38 +0000

    fix struct

--- holberton.h
+++ holberton.h
@@ -4,17 +4,9 @@
 #include <stdarg.h>
 #include <unistd.h>
 
-int _write(char c);
-int _printf(const char *format, ...);
-int loop_str(const char *format, prt_f func[], va_list p);
-int f_char(va_list p);
-int f_string(va_list p);
-int f_percent(va_list p);
-int f_backslash(va_list p);
-
 /**
- * struct ptr - Structure
- * @ptr: pointer
+ * struct prt - Structure
+ * @prt: pointer
  * @f: function pointer
  *
  * Description: Declaration of structure prt.
@@ -25,5 +17,13 @@ typedef struct prt
 	char *prt;
 	int (*f)(va_list);
 } prt_f;
+
+int _write(char c);
+int _printf(const char *format, ...);
+int loop_str(const char *format, prt_f func[], va_list p);
+int f_char(va_list p);
+int f_string(va_list p);
+int f_percent(va_list p);
+int f_backslash(va_list p);
 
 #endif
