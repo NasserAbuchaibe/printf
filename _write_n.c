5ca9e43f87d0b59dbfff54d49ed30464463c536f
kimha1030 <1478@holbertonschool.com>
2020-03-17 21:22:46 -0500

    Change in value of return

--- _write_n.c
+++ _write_n.c
@@ -7,26 +7,26 @@
  */
 int _write_n(int n)
 {
-        int x, count = 0, y = 0;
+	int x, count = 0, y = 0;
 
-        if (n == INT_MIN)
-                n += 1;
-        if (n < 0)
-        {
-                x = -1 * n;
-                _write('-');
-                count++;
-        }
-        else
-        {
-                x = n;
-        }
-        if (x / 10)
-        {
-                y = (_write_n(x / 10));
-        }
-        _write(x % 10 + '0');
-        count++;
-        count += y;
-        return (count);
+	if (n == INT_MIN)
+		n += 1;
+	if (n < 0)
+	{
+		x = -1 * n;
+		_write('-');
+		count++;
+	}
+	else
+	{
+		x = n;
+	}
+	if (x / 10)
+	{
+		y = (_write_n(x / 10));
+	}
+	_write(x % 10 + '0');
+	count++;
+	count += y;
+	return (1);
 }
